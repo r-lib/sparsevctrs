7ba31a327bb5f134509374aa92bf9cc5f3e3c0ff
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-04-30 13:09:37 -0700

    test extractor functions with non-altrep input

--- tests/testthat/test-extractors.R
+++ tests/testthat/test-extractors.R
@@ -1,4 +1,4 @@
-test_that(".positions works", {
+test_that(".positions works with altrep_sparse_real", {
   expect_identical(
     .positions(new_sparse_real(1, 5, 10)),
     5L
@@ -10,7 +10,19 @@ test_that(".positions works", {
   )
 })
 
-test_that(".positions works", {
+test_that(".positions works with numeric vectors", {
+  expect_identical(
+    .positions(c(1, 6, 4, 2)),
+    seq_len(4)
+  )
+
+  expect_identical(
+    .positions(101:200),
+    1:100
+  )
+})
+
+test_that(".values works with altrep_sparse_real", {
   expect_identical(
     .values(new_sparse_real(1, 5, 10)),
     1
@@ -19,5 +31,17 @@ test_that(".positions works", {
   expect_identical(
     .values(new_sparse_real(1:3, 5:7, 10)),
     c(1, 2, 3)
+  )
+})
+
+test_that(".values works with numeric vectors", {
+  expect_identical(
+    .values(c(1, 6, 4, 2)),
+    c(1, 6, 4, 2)
+  )
+
+  expect_identical(
+    .values(101:200),
+    101:200
   )
 })
