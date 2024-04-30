2d25ef2dbbc079d7cec6e215cc8b5f7cd5fb011d
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-04-30 12:16:27 -0700

    use C functions in R extractors

--- R/extractors.R
+++ R/extractors.R
@@ -1,17 +1,7 @@
 .positions <- function(x) {
-  if (inherits(x, "sparse_vector")) {
-    res <- attr(x, "positions")
-  } else {
-    res <- seq_along(x)
-  }
-  res
+  .Call(ffi_altrep_sparse_positions, x)
 }
 
 .values <- function(x) {
-  if (inherits(x, "sparse_vector")) {
-    res <- attr(x, "values")
-  } else {
-    res <- x
-  }
-  res
+  .Call(ffi_altrep_sparse_values, x)
 }
