67f547bf3b37a9e178a8908dfa6b16005455cf75
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-04-30 12:52:13 -0700

    add sparse_vector checks in R extraction functions

--- R/extractors.R
+++ R/extractors.R
@@ -1,7 +1,15 @@
 .positions <- function(x) {
+  if (!is_sparse_vector(x)) {
+    return(seq_along(x))
+  }
+
   .Call(ffi_altrep_sparse_positions, x)
 }
 
 .values <- function(x) {
+  if (!is_sparse_vector(x)) {
+    return(x)
+  }
+
   .Call(ffi_altrep_sparse_values, x)
 }
