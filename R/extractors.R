b42215e132df34d021cf6c793ed3a55965ddab7b
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-05-08 20:56:35 -0700

    export extractor functions

--- R/extractors.R
+++ R/extractors.R
@@ -1,4 +1,22 @@
-.positions <- function(x) {
+#' Sparse vector type checkers
+#' 
+#' @param x value to be checked.
+#'
+#' @examples
+#' x_sparse <- sparse_double(c(pi, 5, 0.1), c(2, 5, 10), 10)
+#' x_dense <- c(0, pi, 0, 0, 0.5, 0, 0, 0, 0, 0.1)
+#' 
+#' sparse_positions(x_sparse)
+#' sparse_values(x_sparse)
+#' 
+#' sparse_positions(x_dense)
+#' sparse_values(x_dense)
+#' @name type-predicates
+NULL
+
+#' @rdname type-predicates
+#' @export
+sparse_positions <- function(x) {
   if (!is_sparse_vector(x)) {
     return(seq_along(x))
   }
@@ -6,7 +24,9 @@
   .Call(ffi_altrep_sparse_positions, x)
 }
 
-.values <- function(x) {
+#' @rdname type-predicates
+#' @export
+sparse_values <- function(x) {
   if (!is_sparse_vector(x)) {
     return(x)
   }
