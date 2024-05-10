5778ebf411d28a24b1edc2ad19b1a972b3522f04
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-05-10 14:10:38 -0700

    refactor out validators for values and positions

--- R/altrep.R
+++ R/altrep.R
@@ -42,130 +42,18 @@ sparse_double <- function(values, positions, length, default = 0) {
     length <- as.integer(length)
   }
 
-
   if (identical(values, NA)) {
     values <- NA_real_
   }
 
-  if (!is.numeric(values)) {
-    cli::cli_abort(
-      "{.arg values} must be a numeric vector, \\
-      not {.obj_type_friendly {values}}."
-    )
-  }
-
-  if (any(is.infinite(values))) {
-    offenders <- which(is.infinite(values))
-    cli::cli_abort(
-      c(
-        x = "{.arg values} must not contain infinite values.",
-        i = "Infinite values at index: {offenders}."
-      )
-    )
-  }
-
-  if (any(is.nan(values))) {
-    offenders <- which(is.nan(values))
-    cli::cli_abort(
-      c(
-        x = "{.arg values} must not contain NaN values.",
-        i = "NaN values at index: {offenders}."
-      )
-    )
-  }
-
+  validate_values_double(values)
+  
   if (is.integer(values)) {
     values <- as.double(values)
   }
-
-  if (!is.numeric(positions)) {
-    cli::cli_abort(
-      "{.arg positions} must be a integer vector, \\
-      not {.obj_type_friendly {positions}}."
-    )
-  }
-
-  if (any(is.infinite(positions))) {
-    offenders <- which(is.infinite(positions))
-    cli::cli_abort(
-      c(
-        x = "{.arg positions} must not contain infinite values.",
-        i = "Infinite values at index: {offenders}."
-      )
-    )
-  }
-
-  if (any(is.nan(positions))) {
-    offenders <- which(is.nan(positions))
-    cli::cli_abort(
-      c(
-        x = "{.arg positions} must not contain NaN values.",
-        i = "NaN values at index: {offenders}."
-      )
-    )
-  }
-
-  if (!is.integer(positions)) {
-    if (any(round(positions) != positions, na.rm = TRUE)) {
-      offenders <- which(round(positions) != positions)
-
-      cli::cli_abort(
-        c(
-          x = "{.arg positions} must contain integer values.",
-          i = "Non-integer values at index: {offenders}."
-        )
-      )
-    }
-   
-    positions <- as.integer(positions)
-  }
-
-  len_values <- length(values)
-  len_positions <- length(positions)
-
-  if (len_values != len_positions) {
-    cli::cli_abort(
-      "{.arg value} ({len_values}) and {.arg positions} ({len_positions}) \\
-      must have the same length."
-    )
-  }
-
-  if (anyDuplicated(positions) > 0) {
-    offenders <- which(duplicated(positions))
-
-    cli::cli_abort(
-      c(
-        x = "{.arg positions} must not contain any duplicate values.",
-        i = "Duplicate values at index: {offenders}."
-      )
-    )
-  }
-
-  if (is.unsorted(positions)) {
-    cli::cli_abort(
-      "{.arg positions} must be sorted in increasing order."
-    )
-  }
-
-  if (len_positions > 0 && max(positions) > length) {
-    offenders <- which(positions > length)
-    cli::cli_abort(
-      c(
-        x = "{.arg positions} value must not be larger than {.arg length}.",
-        i = "Offending values at index: {offenders}."
-      )
-    )
-  }
-
-  if (len_positions > 0 && min(positions) < 1) {
-    offenders <- which(positions < 1)
-    cli::cli_abort(
-      c(
-        x = "{.arg positions} value must positive.",
-        i = "Non-positive values at index: {offenders}."
-      )
-    )
-  }
+  
+  validate_positions(positions, length, len_values = length(values))
+  positions <- as.integer(positions)
 
   if (any(values == default, na.rm = TRUE)) {
     offenders <- which(values == default)
---
+++ R/validate-input.R
@@ -0,0 +1,133 @@
+validate_positions <- function(positions, 
+                               length,
+                               len_values, 
+                               call = rlang::caller_env()) {
+  if (!is.numeric(positions)) {
+    cli::cli_abort(
+      "{.arg positions} must be a integer vector, \\
+      not {.obj_type_friendly {positions}}.",
+      call = call
+    )
+  }
+
+  if (any(is.infinite(positions))) {
+    offenders <- which(is.infinite(positions))
+    cli::cli_abort(
+      c(
+        x = "{.arg positions} must not contain infinite values.",
+        i = "Infinite values at index: {offenders}."
+      ),
+      call = call
+    )
+  }
+
+  if (any(is.nan(positions))) {
+    offenders <- which(is.nan(positions))
+    cli::cli_abort(
+      c(
+        x = "{.arg positions} must not contain NaN values.",
+        i = "NaN values at index: {offenders}."
+      ),
+      call = call
+    )
+  }
+
+  if (!is.integer(positions)) {
+    if (any(round(positions) != positions, na.rm = TRUE)) {
+      offenders <- which(round(positions) != positions)
+
+      cli::cli_abort(
+        c(
+          x = "{.arg positions} must contain integer values.",
+          i = "Non-integer values at index: {offenders}."
+        ),
+        call = call
+      )
+    }
+  }
+
+  len_positions <- length(positions)
+
+  if (len_values != len_positions) {
+    cli::cli_abort(
+      "{.arg value} ({len_values}) and {.arg positions} ({len_positions}) \\
+      must have the same length.",
+      call = call
+    )
+  }
+
+  if (anyDuplicated(positions) > 0) {
+    offenders <- which(duplicated(positions))
+
+    cli::cli_abort(
+      c(
+        x = "{.arg positions} must not contain any duplicate values.",
+        i = "Duplicate values at index: {offenders}."
+      ),
+      call = call
+    )
+  }
+
+  if (is.unsorted(positions)) {
+    cli::cli_abort(
+      "{.arg positions} must be sorted in increasing order.",
+      call = call
+    )
+  }
+
+  if (len_positions > 0 && max(positions) > length) {
+    offenders <- which(positions > length)
+    cli::cli_abort(
+      c(
+        x = "{.arg positions} value must not be larger than {.arg length}.",
+        i = "Offending values at index: {offenders}."
+      ),
+      call = call
+    )
+  }
+
+  if (len_positions > 0 && min(positions) < 1) {
+    offenders <- which(positions < 1)
+    cli::cli_abort(
+      c(
+        x = "{.arg positions} value must positive.",
+        i = "Non-positive values at index: {offenders}."
+      ),
+      call = call
+    )
+  }
+}
+
+validate_values_double <- function(values, call = rlang::caller_env()) {
+  if (!is.numeric(values)) {
+    cli::cli_abort(
+      "{.arg values} must be a numeric vector, \\
+      not {.obj_type_friendly {values}}.",
+      call = call
+    )
+  }
+
+  if (any(is.infinite(values))) {
+    offenders <- which(is.infinite(values))
+    cli::cli_abort(
+      c(
+        x = "{.arg values} must not contain infinite values.",
+        i = "Infinite values at index: {offenders}."
+      ),
+      call = call
+    )
+  }
+
+  if (any(is.nan(values))) {
+    offenders <- which(is.nan(values))
+    cli::cli_abort(
+      c(
+        x = "{.arg values} must not contain NaN values.",
+        i = "NaN values at index: {offenders}."
+      ),
+      call = call
+    )
+  }
+
+
+}
