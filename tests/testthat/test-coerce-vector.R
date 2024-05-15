de8e13e4fccb9e5164ce6697971137bebb9607e0
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-05-14 18:27:16 -0700

    add as_sparse_integer()

--- tests/testthat/test-coerce-vector.R
+++ tests/testthat/test-coerce-vector.R
@@ -11,3 +11,17 @@ test_that("as_sparse_double works", {
   expect_true(is_sparse_double(x_sparse))
   expect_identical(x_sparse, as.numeric(x_dense))
 })
+
+test_that("as_sparse_integer works", {
+  x_dense <- c(3, 0, 2, 0, 0, 0, 4, 0, 0, NA)
+  x_sparse <- as_sparse_integer(x_dense)
+
+  expect_true(is_sparse_integer(x_sparse))
+  expect_identical(x_sparse, as.integer(x_dense))
+
+  x_dense <- c(3L, 0L, 2L, 0L, 0L, 0L, 4L, 0L, 0L, NA)
+  x_sparse <- as_sparse_integer(x_dense)
+
+  expect_true(is_sparse_integer(x_sparse))
+  expect_identical(x_sparse, x_dense)
+})
