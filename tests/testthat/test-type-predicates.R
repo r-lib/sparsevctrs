7c6556b9d12c44af0cac6da60695dbf09e5a1411
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-05-21 17:08:39 -0700

    add is_sparse_numeric

--- tests/testthat/test-type-predicates.R
+++ tests/testthat/test-type-predicates.R
@@ -7,6 +7,15 @@ test_that("is_sparse_vector works", {
   expect_false(is_sparse_vector(NULL))
 })
 
+test_that("is_sparse_numeric works", {
+  expect_true(is_sparse_numeric(sparse_double(1, 1, 1)))
+  expect_true(is_sparse_numeric(sparse_integer(1, 1, 1)))
+
+  expect_false(is_sparse_numeric(c(1, 1, 1)))
+  expect_false(is_sparse_numeric(1:10))
+  expect_false(is_sparse_numeric(NULL))
+})
+
 test_that("is_sparse_double works", {
   expect_true(is_sparse_double(sparse_double(1, 1, 1)))
 
@@ -29,4 +38,12 @@ test_that("is_sparse_character works", {
   expect_false(is_sparse_character(c(1, 1, 1)))
   expect_false(is_sparse_character(1:10))
   expect_false(is_sparse_character(NULL))
+})
+
+test_that("is_sparse_logical works", {
+  expect_true(is_sparse_logical(sparse_logical(TRUE, 1, 1)))
+
+  expect_false(is_sparse_logical(c(1, 1, 1)))
+  expect_false(is_sparse_logical(1:10))
+  expect_false(is_sparse_logical(NULL))
 })
