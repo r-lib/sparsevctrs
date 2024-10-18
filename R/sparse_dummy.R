e16bef30e7be16043129f66f1f8bdc7ed771a765
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-10-17 22:10:30 -0700

    add prelim documentation of sparse_dummy()

--- R/sparse_dummy.R
+++ R/sparse_dummy.R
@@ -1,8 +1,25 @@
-sparse_dummy <- function(x, lvls = NULL) {
-  if (is.null(lvls)) {
-    lvls <- levels(x)
+#' Generate sparse dummy variables
+#' 
+#' @param x A factor.
+#' 
+#' @details
+#' Only factor variables can be used with [sparse_dummy()]. A call to 
+#' `as.factor()` would be required for any other type of data.
+#'
+#' @return A list of sparse integer dummy variables.
+#' 
+#' @examples
+#' x <- factor(c("a", "a", "b", "c", "d", "b"))
+#' 
+#' sparse_dummy(x)
+#' @export
+sparse_dummy <- function(x) {
+  if (!is.factor(x)) {
+    cli::cli_abort("{.arg x} must be a factor, not {.obj_type_friendly {x}}.")
   }
-  
+
+  lvls <- levels(x)
+
   x <- as.integer(x)
   
   counts <- tabulate(x)
