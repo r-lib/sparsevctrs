f3909663a53a535a4d09739bec13f8a4a1ef209c
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-03-04 14:19:50 -0800

    add type arg to sparse_is_na()

--- R/sparse_is_na.R
+++ R/sparse_is_na.R
@@ -1,6 +1,9 @@
 #' Detect Pressence of Missing Values
 #'
 #' @param x A sparse vector.
+#' @param type A single string. Most be one of `logical` or `integer`.
+#' Determines the resulting vector. If `type = integer` then a sparse vector is
+#' returned.
 #'
 #' @details
 #' This function, as with any of the other helper functions assumes that the
@@ -11,22 +14,38 @@
 #'
 #' @seealso [sparse_which_na()]
 #'
-#' @return A logical vector.
+#' @return A logical vector or sparse integer vector.
 #'
 #' @examples
 #' sparse_is_na(
 #'   sparse_double(c(10, NA, 11), c(1, 50, 111), 1000)
 #' )
+#'
+#' sparse_is_na(
+#'   sparse_double(c(10, NA, 11), c(1, 50, 111), 1000),
+#'   type = "integer"
+#' )
 #' @export
-sparse_is_na <- function(x) {
+sparse_is_na <- function(x, type = "logical") {
   values <- sparse_values(x)
 
-  res <- logical(length(x))
-
   na_values <- is.na(values)
-  if (any(na_values)) {
-    positions <- sparse_positions(x)
-    res[positions[na_values]] <- TRUE
+  positions <- sparse_positions(x)
+  positions <- positions[na_values]
+
+  if (type == "logical") {
+    res <- logical(length(x))
+
+    if (any(na_values)) {
+      res[positions] <- TRUE
+    }
+  } else {
+    res <- sparse_integer(
+      rep(1, length(positions)),
+      positions,
+      length(x),
+      sparse_default(x)
+    )
   }
 
   res
